2de4a40defa7fbc978ed67b4d813ed8bbcdb13a6
Piyush <piyushpatel.love6@gmail.com>
2017-12-19 13:46:06 +0530

    session management jwt

---
+++ routes/session_jwtauth.js
@@ -0,0 +1,8 @@
+var express = require('express'),
+    session = require('express-session');
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('././utils/config'); // get our config file
+
+var router = express.Router();
+
+// https://github.com/adnanrahic/securing-restful-apis-with-jwt
