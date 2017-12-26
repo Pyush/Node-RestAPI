0e0149223bc0b72a64d30535f023100703fe47fa
Piyush <piyushpatel.love6@gmail.com>
2017-12-26 18:32:02 +0530

    mail sent

--- routes/urlscrap.js
+++ routes/urlscrap.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var urlMetadata = require('url-metadata');
 var sendMail = require('././utils/SendMail');
+var formidable = require('formidable');
+var fs = require('fs');
+
+// https://github.com/felixge/node-formidable
 
 /* GET users listing. */
 router.post('/get', function(req, res, next) {
@@ -20,5 +24,50 @@ router.post('/get', function(req, res, next) {
 });
 
 router.post('/sendMail',sendMail);
+
+router.post('/profile',function (req,res,next) {
+
+    var form = new formidable.IncomingForm();
+
+    form.multiples = true;
+
+    //console.log(req);
+    // form.parse analyzes the incoming stream data, picking apart the different fields and files for you.
+    form.parse(req, function(err, fields, files) {
+        if (err) {
+            // Check for and handle any errors here.
+            console.error(err.message);
+            return;
+        }
+
+        files.attachment.forEach(function (element, index, array) {
+            fs.readFile(element.path, function (err, data) {
+                var newPath = 'C:/Users/Piyush/' + element.name;
+                fs.writeFile(newPath, data, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            });
+        });
+
+
+        /*var oldpath = files.filetoupload.path;
+        var newpath = 'C:/Users/Piyush' + files.filetoupload.name;
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
+        });*/
+
+        //res.writeHead(200, {'content-type': 'text/plain'});
+        //res.write('received upload:\n\n');
+        //console.log(fields);
+        console.log(files);
+        // This last line responds to the form submission with a list of the parsed data and files.
+        res.json({"code" : 100, "status" : "received", "view": fields});
+        res.end();
+    });
+});
 
 module.exports = router;
