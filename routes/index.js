d516f83c4f55c17403450b227a828d9d1e6d80ea
Piyush <piyushpatel.love6@gmail.com>
2017-12-27 16:51:58 +0530

    babel setup configuration

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,11 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// https://babeljs.io/docs/setup#installation
+// https://blog.jetbrains.com/webstorm/2015/05/ecmascript-6-in-webstorm-transpiling#babelfilewatcher
+// install babel
+// npm install --save-dev babel-cli
+// File | Settings | Tools | File Watchers
+// In Preferences - Tools - File watchers, click + button and select Babel file watcher from the list.
+// Lastly, in Languages & Frameworks - JavaScript - JavaScript language version, choose ECMAScript 6.
